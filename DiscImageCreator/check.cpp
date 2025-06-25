a7b5f1785893f1b5dcbcba3d26df401d807c9227
fuzzball <github@fuckin.rash.jp>
2025-06-25 14:37:34 +0900

    Update check.cpp

--- DiscImageCreator/check.cpp
+++ DiscImageCreator/check.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <windows.h>
 #include <ntddmmc.h>
 #include "struct.h"
 #include "check.h"
