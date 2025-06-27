5baeaaea64e8840c8a2716ef25668d26b715c9bd
fuzzball <github@fuckin.rash.jp>
2025-06-27 17:36:28 +0900

    Update get.cpp

--- DiscImageCreator/get.cpp
+++ DiscImageCreator/get.cpp
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifdef _WIN32
+#include <shlwapi.h>
+#include <time.h>
+#endif
 #include "struct.h"
 #include "check.h"
 #include "convert.h"
