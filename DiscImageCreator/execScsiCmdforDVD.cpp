41837704714a059bc9ce43414e7aa58106b3e6e3
fuzzball <github@fuckin.rash.jp>
2025-06-27 17:35:30 +0900

    Update execScsiCmdforDVD.cpp

--- DiscImageCreator/execScsiCmdforDVD.cpp
+++ DiscImageCreator/execScsiCmdforDVD.cpp
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifdef _WIN32
+#include <ntddmmc.h>
+#endif
 #include "struct.h"
 #include "check.h"
 #include "convert.h"
