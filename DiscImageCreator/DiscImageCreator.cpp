8dde7664327c8e65ea932a7c10482a68c3de2e2b
fuzzball <github@fuckin.rash.jp>
2025-06-29 15:58:22 +0900

    Update DiscImageCreator.cpp

--- DiscImageCreator/DiscImageCreator.cpp
+++ DiscImageCreator/DiscImageCreator.cpp
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifdef _WIN32
-#include "stdafx.h"
-#endif
 #include "buildDateTime.h"
 #include "struct.h"
 #include "calcHash.h"
